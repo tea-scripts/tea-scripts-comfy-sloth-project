68b6c097a9343fbabb2a1d35e7dc049de9218bbb
teamie <timmieprince@gmail.com>
2022-05-16 20:05:48 +0800

    initializing project

---
+++ src/components/Error.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Error = () => {
+  return (
+    <div className="section-center section text-center">
+      <h2>there was an error</h2>
+    </div>
+  );
+};
+
+export default Error;
---
+++ src/reducers/products_reducer.js
@@ -0,0 +1,48 @@
+import {
+  SIDEBAR_OPEN,
+  SIDEBAR_CLOSE,
+  GET_PRODUCTS_BEGIN,
+  GET_PRODUCTS_SUCCESS,
+  GET_PRODUCTS_ERROR,
+  GET_SINGLE_PRODUCT_BEGIN,
+  GET_SINGLE_PRODUCT_SUCCESS,
+  GET_SINGLE_PRODUCT_ERROR,
+} from '../actions';
+
+const products_reducer = (state, action) => {
+  if (action.type === SIDEBAR_OPEN) {
+    return { ...state, isSidebarOpen: true };
+  }
+  if (action.type === SIDEBAR_CLOSE) {
+    return { ...state, isSidebarOpen: false };
+  }
+  if (action.type === GET_PRODUCTS_BEGIN) {
+    return { ...state, products_loading: true };
+  }
+  if (action.type === GET_PRODUCTS_SUCCESS) {
+    const featured_products = action.payload.filter(
+      (product) => product.featured === true
+    );
+    return {
+      ...state,
+      products_loading: false,
+      products: action.payload,
+      featured_products,
+    };
+  }
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return { ...state, products_loading: false, products_error: true };
+  }
+  if (action.type === GET_SINGLE_PRODUCT_BEGIN) {
+    return { ...state, product_loading: true, product_error: false };
+  }
+  if (action.type === GET_SINGLE_PRODUCT_SUCCESS) {
+    return { ...state, product_loading: false, product: action.payload };
+  }
+  if (action.type === GET_SINGLE_PRODUCT_ERROR) {
+    return { ...state, product_error: true, product_loading: false };
+  }
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default products_reducer;
